23fadd91d73baafe8eb749c596bfd48a425ed980
Elyse <26639800+elyse0@users.noreply.github.com>
2025-10-05 17:15:09 -0600

    feat: add search routes

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
 import captionsRouter from '@/modules/captions/captions-router.js'
+import searchRouter from '@/modules/search/search-router.js'
 
 const router = express.Router({ mergeParams: true }) as express.Router;
 
-router.use('/captions', captionsRouter)
+router.use('/captions', captionsRouter);
+router.use('/search', searchRouter);
 
 export default router
